140888cfa116e71f287bd6e4d0f85fa93587b1a6
Stefan Garces <stefan.garces@volvo.com>
2023-12-01 15:37:10 +0100

    disabled ctrl+z

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -15,6 +15,7 @@ keymap('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic mess
 keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 keymap('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 keymap('n', '<leader>a', 'ggVG', { desc = 'Select all shortcut' })
+
 -- Enable to use Shift + hjkl to move around in Insert Mode
 keymap('i', '<S-h>', '<Left>', {noremap = true, silent = true})
 keymap('i', '<S-j>', '<Down>', {noremap = true, silent = true})
@@ -26,6 +27,9 @@ keymap({'v', 'i', 'n'}, '<Up>', '<Nop>', {noremap = true, silent = true})
 keymap({'v', 'i', 'n'}, '<Down>', '<Nop>', {noremap = true, silent = true})
 keymap({'v', 'i', 'n'}, '<Left>', '<Nop>', {noremap = true, silent = true})
 keymap({'v', 'i', 'n'}, '<Right>', '<Nop>', {noremap = true, silent = true})
+
+-- Disabling ctrl + z
+keymap({'v', 'i', 'n'}, '<C-z>', '<Nop>', {noremap = true, silent = true})
 
 -- keymaps for opening telescope-file-browser
 keymap(
